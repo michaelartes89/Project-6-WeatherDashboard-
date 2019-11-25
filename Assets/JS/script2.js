40bb6cba0610bcdf176659a2e503f7627d711f06
MDBA <artes.mike@gmail.com>
2019-11-25 01:37:01 -0500

    declaring variables to target html elements

---
+++ Assets/JS/script2.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    //create an array to hold previous city searches
+    var previousCitySearches =[];
+    //create a variable to hold current search input as a string
+    var currentCitySearch ="";
+    //create variables to target html elements
+    var cityUserInput = $("#city-input");
+    var cityInputButton = $("#city-input-button");
+    var cityTodaysHumidity = $("#todays-humidity");
+    var cityTodaysTemperature = $("#todays-temperature");
+    var cityTodaysWindSpeed = $("#todays-wind-speed");
+    var cityTodaysUVIndex = $("#todays-uv-index");
+    var dayOneDisplayDiv = $("#day-one-display");
+    var dayTWoDisplayDiv = $("#day-two-display");
+    var dayThreeDisplayDiv = $("#day-three-display");
+    var dayFourDisplayDiv = $("#day-four-display");
+    var dayFiveDisplayDiv = $("#day-five-display");
+    var mainCityInfoDiv = $("#main-city-info");
+    var previousCitySearchesDiv = $("#previous-searches");
+
+
+
+}
